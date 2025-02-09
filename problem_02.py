86e08d02a1efac81f61f9fcb1fd53b2595f0aa36
Joshua Tigno <tignojoshua@plmun.edu.ph>
2025-02-09 09:01:48 +0800

    Initial Project Files

--- problem_02.py
+++ problem_02.py
@@ -4,11 +4,7 @@
 # Square the Number Program
 
 def main():
-    print("Square the Number Program")
-
-    number = int(input("Enter a number: "))
-
-    print(f"The square of {number} is {(number*number):.2f}")
+    pass # replace this line with all of your code
 
 if __name__ == "__main__":
     main()
